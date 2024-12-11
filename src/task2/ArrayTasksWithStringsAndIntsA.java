5bca6c6167823d6edb50766169f20cfcc5f9313d
Olena Konovalova <helen.konovalova@gmail.com>
2024-12-12 00:01:48 +0200

    Задачи 1-10.

--- src/task2/ArrayTasksWithStringsAndIntsA.java
+++ src/task2/ArrayTasksWithStringsAndIntsA.java
@@ -1,4 +1,4 @@
-package taski2;
+package task2;
 
 public class ArrayTasksWithStringsAndIntsA {
 
@@ -31,9 +31,7 @@ public class ArrayTasksWithStringsAndIntsA {
         String target = "banana";
         for (int i = 0; i < array.length; i++) {
             if (array[i] == target){
-
             }
-
         }
         System.out.println(true);
     }
@@ -50,7 +48,6 @@ public class ArrayTasksWithStringsAndIntsA {
                     break;
                 }
             }
-
         }
         System.out.println(count);
     }
@@ -68,52 +65,91 @@ public class ArrayTasksWithStringsAndIntsA {
                     count++;
                 }
             }
-if (count > maxCount){
-    maxCount = count;
-    maxString = array[i];
-}
+                if (count > maxCount){
+                    maxCount = count;
+                    maxString = array[i];
+                }
         }
         System.out.println(maxString);
-
     }
 
     // 6. Удалить из массива строк все строки, содержащие заданное слово.
     public static void task6() {
         String[] array = {"apple pie", "banana bread", "cherry tart"};
         String target = "bread";
+        String[] array = {"apple pie", "banana bread", "cherry tart"};
+        String target = "bread";
+        String[] newArray = new String[array.length];
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i].contains(target)){
+            if (!array[i].contains(target)){
+                newArray[count] = array[i];
                 count++;
             }
         }
-        String[] newArray = new String[array.length - count];
+        for (int i = 0; i < count; i++) {
+            System.out.println(newArray[i]);
+        }
     }
 
 
     // 7. Найти минимальный элемент массива целых чисел.
     public static void task7() {
         int[] array = {3, 1, 4, 1, 5};
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] < min){
+                min = array[i];
+            }
+            System.out.println(min);
 
     }
 
     // 8. Найти строку с минимальной длиной.
     public static void task8() {
         String[] array = {"cat", "elephant", "dog"};
+            String minString = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if(array[i].length() < minString.length()){
+                    minString = array[i];
+                }
+            }
+            System.out.println(minString); //Выводит только Cat. Не знаю, как вывести еще Dog. Еще один цикл нужен скорее всего
 
     }
 
     // 9. Подсчитать количество четных чисел в массиве.
     public static void task9() {
         int[] array = {1, 2, 3, 4, 5};
+        int count = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
 
-    }
 
     // 10. Найти строку, которая встречается чаще всего.
     public static void task10() {
-        String[] array = {"apple", "banana", "apple", "cherry", "banana", "banana"};
-        String mostFrequent = "";
-        int maxCount = 0;
+            String[] array = {"apple", "banana", "apple", "cherry", "banana", "banana"};
+            String mostFrequent = "";
+            int maxCount = 0;
+            for (int i = 0; i < array.length; i++) {
+                int count = 0;
+                for (int j = 0; j < array.length; j++) {
+                    if (array[i].equals(array[j])) {
+                        count++;
+                    }
+                }
+                if (count > maxCount){
+                    maxCount = count;
+                    mostFrequent = array[i];
+
+                }
+            }
+            System.out.println(mostFrequent);
     }
 
     public static void main(String[] args) {
